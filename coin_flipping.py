e124f41e9abde67a6c1c82e356fca91e55869655
oriont <elijahotarr@gmail.com>
2020-07-22 10:19:27 -0700

    probability with coin flipping

---
+++ coin_flipping.py
@@ -0,0 +1,26 @@
+import math
+import random
+
+
+def probability(num_heads, num_flips):
+    possible_outcomes = 2**num_flips
+    outcomes_permutation = math.factorial(
+        num_flips) / (math.factorial(num_flips - num_heads) * math.factorial(num_heads))
+    return outcomes_permutation / possible_outcomes
+
+
+def monte_carlo_probability(num_heads, num_flips):
+    total_trials = 1000
+    match_flips = 0
+    for _ in range(total_trials):
+        trial = [random.randint(0, 1) for _ in range(num_flips)]
+        if trial.count(1) == num_heads:
+            match_flips += 1
+
+    return match_flips / total_trials
+
+
+print(probability(5, 8))
+print(monte_carlo_probability(5, 8))
+print(monte_carlo_probability(5, 8))
+print(monte_carlo_probability(5, 8))
