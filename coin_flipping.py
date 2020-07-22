d1c1216386aa89934c0aba4ef1877232b616481d
oriont <elijahotarr@gmail.com>
2020-07-22 14:35:58 -0700

    add a factorial function

--- coin_flipping.py
+++ coin_flipping.py
@@ -1,11 +1,10 @@
-import math
 import random
 
 
 def probability(num_heads, num_flips):
     possible_outcomes = 2**num_flips
-    outcomes_permutation = math.factorial(
-        num_flips) / (math.factorial(num_flips - num_heads) * math.factorial(num_heads))
+    outcomes_permutation = factorial(
+        num_flips) / (factorial(num_flips - num_heads) * factorial(num_heads))
     return outcomes_permutation / possible_outcomes
 
 
@@ -18,6 +17,10 @@ def monte_carlo_probability(num_heads, num_flips):
             match_flips += 1
 
     return match_flips / total_trials
+
+
+def factorial(num):
+    return 1 if num in [0, 1] else factorial(num-1) * num
 
 
 print("Normal Probability Using Combination")
