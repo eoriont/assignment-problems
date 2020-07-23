361c8fbf0be18f8fbad36ab4329b30dbc5a4d39c
oriont <elijahotarr@gmail.com>
2020-07-23 16:31:41 -0700

    add pyplot

--- coin_flipping.py
+++ coin_flipping.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+plt.style.use("bmh")
 
 
 def probability(num_heads, num_flips):
@@ -8,24 +10,51 @@ def probability(num_heads, num_flips):
     return outcomes_permutation / possible_outcomes
 
 
-def monte_carlo_probability(num_heads, num_flips):
-    total_trials = 1000
-    match_flips = 0
+def monte_carlo_trials(num_flips, total_trials):
+    trials = {}
     for _ in range(total_trials):
         trial = [random.randint(0, 1) for _ in range(num_flips)]
-        if trial.count(1) == num_heads:
-            match_flips += 1
+        heads = trial.count(1)
+        if heads in trials:
+            trials[heads] += 1
+        else:
+            trials[heads] = 1
+    return {x: y/total_trials for x, y in trials.items()}
 
-    return match_flips / total_trials
+
+def monte_carlo_probability(num_heads, num_flips):
+    match_flips = monte_carlo_trials(num_flips, 1000)[num_heads]
+    return match_flips
 
 
 def factorial(num):
     return 1 if num in [0, 1] else factorial(num-1) * num
 
 
-print("Normal Probability Using Combination")
-print(probability(5, 8))
-print("Monte Carlo Probability Trials")
-print(monte_carlo_probability(5, 8))
-print(monte_carlo_probability(5, 8))
-print(monte_carlo_probability(5, 8))
+def plot_probabilities():
+    # True Probabilities
+    probabilities = [{x: probability(x, 10) for x in range(11)}]
+    # Monte Carlo Probabilities
+    probabilities += [dict(sorted(monte_carlo_trials(10, 1000).items()))
+                      for _ in range(5)]
+    default_dict = {x: 0 for x in range(11)}
+    for i, probs in enumerate(probabilities):
+        x_vals, y_vals = zip(
+            *dict(list(default_dict.items()) + list(probs.items())).items())
+        line_width = 2.5 if i == 0 else 0.75
+        plt.plot(x_vals, y_vals, linewidth=line_width)
+
+    plt.legend(["True", "MC 1", "MC 2"])
+    plt.xlabel("Number of Heads")
+    plt.ylabel("Probability")
+    plt.title("True Distribution vs Monte Carlo Simulations for 10 Coin Flips")
+    plt.show()
+
+
+plot_probabilities()
+# print("Normal Probability Using Combination")
+# print(probability(5, 8))
+# print("Monte Carlo Probability Trials")
+# print(monte_carlo_probability(5, 8))
+# print(monte_carlo_probability(5, 8))
+# print(monte_carlo_probability(5, 8))
