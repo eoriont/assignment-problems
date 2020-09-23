8aaf94d4752e3a013112bc7dbb4fcc3f6e2b1b9a
oriont <elijahotarr@gmail.com>
2020-09-22 19:44:53 -0700

    Use a more accurate method for plotting a graph for the euler estimator

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -10,25 +10,32 @@ class EulerEstimator:
         return self.derivative(self.point[0])
 
     def step_forward(self, step_size):
-        x, y = self.point
-        self.point = x+step_size, y + step_size*self.calc_derivative_at_point()
+        x = round(self.point[0]+step_size, 10)
+        y = round(self.point[1] + step_size *
+                  self.calc_derivative_at_point(), 10)
+        self.point = x, y
 
     def go_to_input(self, x_val, step_size):
         if x_val < self.point[0]:
             step_size *= -1
         while round(self.point[0], 10) != x_val:
-            if abs(self.point[0] - x_val) <= step_size:
+            if abs(round(self.point[0] - x_val, 10)) <= step_size:
                 step_size = x_val-self.point[0]
             self.step_forward(step_size)
-            if self.point[0] < -10:
-                break
 
     def plot(self, r, step_size, filename):
-        x_data = [i for i in range(*r)]
+        x_data = [round(r[0]+i*step_size, 10)
+                  for i in range(int((r[1]-r[0])/step_size))]
+        given_index = x_data.index(self.point[0])
+        given_point = self.point
         y_data = []
-        for x in x_data:
+        for x in x_data[:given_index][::-1]:
+            self.go_to_input(x, step_size)
+            y_data.insert(0, self.point[1])
+        self.point = given_point
+        for x in x_data[given_index:]:
             self.go_to_input(x, step_size)
             y_data.append(self.point[1])
-        plt.scatter(x_data, y_data, zorder=2, color='black')
-        plt.plot(x_data, y_data, zorder=1)
+
+        print(min(y_data))        plt.plot(x_data, y_data, zorder=1)
         plt.savefig(filename)
--- src/test_euler_estimator.py
+++ src/test_euler_estimator.py
@@ -41,3 +41,14 @@ euler.go_to_input(3, step_size=0.5)
 
 do_assert("euler point after go_to_input", _round(euler.point),
           (3, 9.29))
+
+euler = EulerEstimator(derivative=(lambda x: x+1),
+                       point=(1, 4))
+
+euler.plot([-5, 5], step_size=0.1, filename='plot.png')
+
+# generates a plot of the function for x-values in the
+# interval [-5,5] separated by a length of step_size
+
+# for this example, the plot should look like the parabola
+# y = 0.5x^2 + x + 2.5
