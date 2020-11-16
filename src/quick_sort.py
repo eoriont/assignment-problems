95edb55f2983f612546a33896441a13c493744ef
oriont <elijahotarr@gmail.com>
2020-11-15 22:25:01 -0800

    add efficient quicksort

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,5 +1,5 @@
-
-def quick_sort(arr):
+# Inefficient
+def quick_sort2(arr):
     if len(arr) == 0:
         return []
     pivot, l = arr[-1], [[], []]
@@ -10,6 +10,23 @@ def quick_sort(arr):
             l[1].append(i)
     l[0], l[1] = quick_sort(l[0]), quick_sort(l[1])
     return l[1] + [pivot] + l[0]
+
+# Modifies in place
+
+
+def quick_sort(arr, l=0, r=-1):
+    r = len(arr) if r == -1 else r
+    if r - l in (0, 1):
+        return
+    i = l+1
+    for x in range(l, r):
+        if arr[l] > arr[x]:
+            arr[i], arr[x] = arr[x], arr[i]
+            i += 1
+    arr[i-1], arr[l] = arr[l], arr[i-1]
+    quick_sort(arr, l, i)
+    quick_sort(arr, i, r)
+    return arr
 
 
 if __name__ == "__main__":
