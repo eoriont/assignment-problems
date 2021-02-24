cb3177c8cb0534d3041f56844b48dce8f4de66be
oriont <elijahotarr@gmail.com>
2021-02-23 20:33:42 -0800

    hash table update into class

--- src/hash_table.py
+++ src/hash_table.py
@@ -1,43 +1,45 @@
-def hash(array, string):
-    return sum(ord(char)-97 for char in string) % len(array)
+class HashTable:
+    def __init__(self, num_buckets=5):
+        self.num_buckets = num_buckets
+        self.buckets = [[] for _ in range(num_buckets)]
 
-def insert(array, key, value):
-    bucket = hash(array, key)
-    array[bucket].append((key, value))
+    def insert(self, key, value):
+        self.buckets[self.hash_function(key)].append((key, value))
 
-def find(array, key):
-    bucket = hash(array, key)
-    return next((v for k, v in array[bucket] if k == key), None)
+    def hash_function(self, string):
+        return sum(ord(char)-97 for char in string) % self.num_buckets
+
+    def find(self, key):
+        return next((v for k, v in self.buckets[self.hash_function(key)] if k == key), None)
 
 if __name__ == "__main__":
-    array = [[], [], [], [], []]
-    print(array)
-    # [[], [], [], [], []]
+    ht = HashTable(num_buckets = 3)
+    print(ht.buckets)
+    # [[], [], []]
+    print(ht.hash_function('cabbage'))
+    # 2    (because 2+0+1+1+0+6+4 mod 3 = 14 mod 3 = 2)
 
-    insert(array, 'a', [0,1])
-    insert(array, 'b', 'abcd')
-    insert(array, 'c', 3.14)
-    print(array)
-    # [[('a',[0,1])], [('b','abcd')], [('c',3.14)], [], []]
+    ht.insert('cabbage', 5)
+    print(ht.buckets)
+    # [[], [], [('cabbage',5)]]
 
-    insert(array, 'd', 0)
-    insert(array, 'e', 0)
-    insert(array, 'f', 0)
-    print(array)
-    # [[('a',[0,1]), ('f',0)], [('b','abcd')], [('c',3.14)], [('d',0)], [('e',0)]]
-    # Test your code as follows:
+    ht.insert('cab', 20)
+    print(ht.buckets)
+    # [[('cab', 20)], [], [('cabbage',5)]]
 
-    array = [[], [], [], [], []]
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    for i, char in enumerate(alphabet):
-        key = 'someletters'+char
-        value = [i, i**2, i**3]
-        insert(array, key, value)
+    ht.insert('c', 17)
+    print(ht.buckets)
+    # [[('cab', 20)], [], [('cabbage',5), ('c',17)]]
 
-    for i, char in enumerate(alphabet):
-        key = 'someletters'+char
-        output_value = find(array, key)
-        desired_value = [i, i**2, i**3]
-        assert output_value == desired_value
+    ht.insert('ac', 21)
+    print(ht.buckets)
+    # [[('cab', 20)], [], [('cabbage',5), ('c',17), ('ac', 21)]]
 
-    print("Passed!")
+    print(ht.find('cabbage'))
+    # 5
+    print(ht.find('cab'))
+    # 20
+    print(ht.find('c'))
+    # 17
+    print(ht.find('ac'))
+    # 21
